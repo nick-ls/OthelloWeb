ed799ce72c2bccbb7ce24dc84ca0930b62ce13f6
Nick <nick-ls@users.noreply.github.com>
2024-09-08 14:28:28 -0700

    unbreak things i guess

--- src/public/controller.ts
+++ src/public/controller.ts
@@ -57,7 +57,6 @@ socket.on("joinGame", (msg: Color) => {
 		elems[i].style.display = "none";
 	}*/
 	(<HTMLElement>document.getElementById("start")).style.display = "none";
-	(<HTMLElement>document.getElementById("start")).style.display = "";
 	color = Number(msg);
 	whoseTurn = Color.BLACK;
 });
